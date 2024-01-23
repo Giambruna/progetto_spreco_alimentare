327eac403bb30ccfd6b6a1e283ea051e37252087
giulia <ilgelsonero.an@gmail.com>
2024-01-23 19:34:53 +0100

    aggiunta faq. test su github

---
+++ js.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const answer = button.nextElementSibling;
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+});
+window.onload = function() {
+    alert("JavaScript è abilitato e funzionante!");
+};
+
